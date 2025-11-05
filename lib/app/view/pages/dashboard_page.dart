48158ff4a1b65798f81a977455fad43c7e321484
1BrenoAlv <brenoalves.queirozoficial@gmail.com>
2025-11-05 14:26:15 -0300

    feat: added a new design and started the dashboard page

--- lib/app/view/pages/dashboard_page.dart
+++ lib/app/view/pages/dashboard_page.dart
@@ -1,13 +1,34 @@
+import 'package:fittrack_ui/app/view/const/colors_const.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class DashboardPage extends StatelessWidget {
   const DashboardPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final String name = ModalRoute.of(context)!.settings.arguments as String;
+
     return Scaffold(
-      appBar: AppBar(),
-      body: Column(children: [Text('Dashboard')]),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        automaticallyImplyLeading: false,
+        title: Text(
+          'Olá $name👋',
+          style: GoogleFonts.workSans(
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      extendBodyBehindAppBar: true,
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(gradient: ColorsConst.colorBack),
+        child: Column(),
+      ),
     );
   }
 }
--- lib/app/view/pages/initial_page.dart
+++ lib/app/view/pages/initial_page.dart
@@ -24,87 +24,125 @@ class _InitialPageState extends State<InitialPage> {
     super.dispose();
   }
 
-  void nameUser() {
-    if (_textController.text.trim().isEmpty) {
-      AlertInitial.alertTextEmpty(context);
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('HOme')),
+      appBar: AppBar(backgroundColor: Colors.transparent, elevation: 0),
+      extendBodyBehindAppBar: true,
       body: Container(
         width: double.infinity,
+        height: double.infinity,
         decoration: BoxDecoration(gradient: ColorsConst.colorBack),
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 40.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              AnimatedTextKit(
-                animatedTexts: [
-                  TypewriterAnimatedText(
-                    'Digite seu nome',
-                    textStyle: GoogleFonts.workSans(
-                      fontSize: 24.0,
-                      fontWeight: FontWeight.w700,
-                    ),
-                    speed: Duration(milliseconds: 150),
+
+        child: Center(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 40.0),
+
+              child: Container(
+                padding: EdgeInsets.all(30.0),
+                decoration: BoxDecoration(
+                  color: const Color.fromARGB(59, 10, 86, 173),
+                  borderRadius: BorderRadius.circular(20.0),
+                  border: Border.all(
+                    color: const Color.fromARGB(122, 10, 86, 173),
                   ),
-                ],
-                totalRepeatCount: 1,
-                pause: const Duration(milliseconds: 1000),
-                displayFullTextOnTap: true,
-                stopPauseOnTap: true,
-              ),
-              SizedBox(height: 30),
-              Stack(
-                // FEITO UM EFEITO BORRADO NA FRENTE DA IMAGEM
-                alignment: Alignment.center,
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(30.0),
-                    child: Image.asset(
-                      ImagesConst.imageInitial,
-                      width: 300,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(30.0),
-                      child: BackdropFilter(
-                        filter: ImageFilter.blur(sigmaX: 1.0, sigmaY: 1.0),
-                        child: Container(
-                          decoration: BoxDecoration(
-                            color: const Color.fromARGB(14, 0, 0, 0),
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    AnimatedTextKit(
+                      animatedTexts: [
+                        TypewriterAnimatedText(
+                          'Digite seu nome',
+                          textStyle: GoogleFonts.workSans(
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.w700,
+                            color: Colors.white,
                           ),
-                          width: 300,
-                          height: (300 / 16 * 9),
+                          speed: Duration(milliseconds: 150),
+                        ),
+                      ],
+                      totalRepeatCount: 1,
+                      pause: const Duration(milliseconds: 1000),
+                      displayFullTextOnTap: true,
+                      stopPauseOnTap: true,
+                    ),
+                    SizedBox(height: 30),
+                    Stack(
+                      alignment: Alignment.center,
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(30.0),
+                          child: Image.asset(
+                            ImagesConst.imageInitial,
+                            width: 300,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.all(10.0),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(30.0),
+                            child: BackdropFilter(
+                              filter: ImageFilter.blur(
+                                sigmaX: 1.0,
+                                sigmaY: 1.0,
+                              ),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: const Color.fromARGB(14, 0, 0, 0),
+                                ),
+                                width: 300,
+                                height: (300 / 16 * 9),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 30),
+
+                    TextField(
+                      controller: _textController,
+                      style: TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                        hintText: "Seu nome aqui",
+                        hintStyle: TextStyle(
+                          color: const Color.fromARGB(58, 255, 255, 255),
+                        ),
+                        suffixIcon: IconButton(
+                          onPressed: () {
+                            final String name = _textController.text.trim();
+                            if (name.isEmpty) {
+                              AlertInitial.alertTextEmpty(context);
+                            } else {
+                              Navigator.pushReplacementNamed(
+                                context,
+                                '/dashboard',
+                                arguments: name,
+                              );
+                            }
+                          },
+                          icon: Icon(Icons.send, color: Colors.white70),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          borderSide: BorderSide(
+                            color: const Color.fromARGB(58, 255, 255, 255),
+                          ),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                          borderSide: BorderSide(color: Colors.white),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-
-              SizedBox(height: 30),
-              TextField(
-                controller: _textController,
-                decoration: InputDecoration(
-                  suffixIcon: IconButton(
-                    onPressed: () {
-                      nameUser();
-
-                      Navigator.pushNamed(context, '/dashboard');
-                    },
-                    icon: Icon(Icons.send),
-                  ),
+                  ],
                 ),
               ),
-            ],
+            ),
           ),
         ),
       ),
